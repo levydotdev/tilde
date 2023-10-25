2f4c96fd1f0b294664a7b992bd3bccce4b612c33
Levy Fekete <git@levy.dev>
2023-10-25 16:55:37 +0200

    Small tweaks

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
-echo "[i] Ask for admin permissions"
+IFS=$'\n'
+
+echo "${tty_blue}[i]${tty_reset} Ask for admin permissions"
 sudo -v
 
 # Move to home folder
 cd
 
 # Create local tilde directory
-mkdir -p ~/Workspace/playpen/tilde
+mkdir -p ~/Workspace/playpen/tilde && cd $_
 
 # Install Command Line Tools
 if [[ ! -x /Library/Developer/CommandLineTools/usr/bin/git ]]; then
-  echo "[i] Install macOS Command Line Tools"
+  echo "${tty_blue}[i]${tty_reset} Install macOS Command Line Tools"
   xcode-select --install &> /dev/null
 
   until $(xcode-select --print-path &> /dev/null); do
@@ -22,19 +24,19 @@ fi
 
 # Install homebrew
 if [[ ! -x /usr/local/bin/brew ]]; then
-  echo "[i] Install Homebrew"
+  echo "${tty_blue}[i]${tty_reset} Install Homebrew"
   # instructions: https://brew.sh/
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Clone tilde dotfiles
 if [[ ! -d ~/Workspace/playpen/tilde/.git ]]; then
-    echo "[i] Cloning dotfiles directory"
+    echo "${tty_blue}[i]${tty_reset} Cloning dotfiles directory"
     git clone https://github.com/levydotdev/tilde.git ~/Workspace/playpen/tilde
 fi
 
-echo "[i] Get latest versions of dotfiles"
-cd ~/Workspace/playpen/tilde && git pull origin main
+echo "${tty_blue}[i]${tty_reset} Get latest versions of dotfiles"
+git pull origin main
 
-echo "[i] Done."
+echo "${tty_blue}[i]${tty_reset} Done."
 exit 0
